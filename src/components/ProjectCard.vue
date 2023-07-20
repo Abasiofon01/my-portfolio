8fb9fbe5b3c4168e37a473909ee9f720fc4778f5
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-07-20 10:33:17 +0100

    refactor: refactor project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,68 +1,35 @@
 <template>
-  <a
-    :href="href"
-    rel="noreferrer"
-    target="_blank"
-    class="tw-p-5 md:tw-p-8 tw-rounded-2xl tw-bg-gradient-to-tl from-tw-gray-800 to-tw-gray-700 lg:tw-aspect-square tw-text-gray-400 tw-border tw-border-transparent tw-border-opacity-0 hover:tw-border-gray-700 hover:tw-border-opacity-50 hover:tw-shadow-xl hover:tw-from-gray-900 hover:tw-to-gray-800 tw-transition-all"
-  >
-    <span
-      class="tw-inline-flex tw-items-center tw-justify-center tw-w-14 tw-h-14 tw-bg-gray-800 tw-rounded-lg tw-mb-5"
-      :style="{ backgroundColor: background }"
-    >
-      <q-img :src="icon" class="tw-h-6 tw-w-6" :alt="title" />
-    </span>
-
-    <h3
-      class="tw-text-xl md:tw-text-2xl tw-font-bold tw-text-gray-200 tw-mb-2 md:tw-mb-3"
-    >
-      {{ title }}
-    </h3>
-    <p class="tw-text-sm md:tw-text-base">{{ description }}</p>
-  </a>
-
-  <div
-    class="tw-max-w-sm tw-bg-white tw-border tw-border-gray-200 tw-rounded-lg tw-shadow dark:tw-bg-gray-800 dark:tw-border-gray-700"
-  >
-    <a href="#">
-      <img
-        class="tw-rounded-t-lg"
-        src="https://cdn.quasar.dev/img/parallax2.jpg"
-        alt=""
-      />
-    </a>
-    <div class="tw-p-5">
-      <a href="#">
-        <h5
-          class="tw-mb-2 tw-text-2xl tw-font-bold tw-tracking-tight tw-text-gray-900 dark:tw-text-white"
+  <div class="card-container">
+    <div class="card">
+      <div class="img-content">
+        <img :src="image" alt="" />
+      </div>
+      <div class="content">
+        <span class="title">{{ title }}</span>
+        <span>
+          {{ description }}
+        </span>
+        <q-btn
+          :href="href"
+          class="tw-self-start tw-bg-neutral-500 tw-text-neutral-200"
+          >{{ buttonText }}</q-btn
         >
-          Noteworthy technology acquisitions 2021
-        </h5>
-      </a>
-      <p class="tw-mb-3 tw-font-normal tw-text-gray-700 dark:tw-text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far,
-        in reverse chronological order.
-      </p>
-      <a
-        href="#"
-        class="tw-inline-flex tw-items-center tw-px-3 tw-py-2 tw-text-sm tw-font-medium tw-text-center tw-text-white tw-bg-blue-700 tw-rounded-lg hover:tw-bg-blue-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-blue-300 dark:tw-bg-blue-600 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-800"
-      >
-        Read more
-        <svg
-          class="tw-w-3.5 tw-h-3.5 tw-ml-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </a>
+
+        <div class="tw-pt-4 tw-pb-2">
+          <span
+            class="tw-inline-block tw-bg-neutral-200 tw-rounded-full tw-px-3 tw-py-1 tw-text-sm tw-font-semibold tw-text-neutral-700 tw-mr-2 mb-2"
+            >#photography</span
+          >
+          <span
+            class="tw-inline-block tw-bg-neutral-200 tw-rounded-full tw-px-3 tw-py-1 tw-text-sm tw-font-semibold tw-text-neutral-700 tw-mr-2 mb-2"
+            >#travel</span
+          >
+          <span
+            class="tw-inline-block tw-bg-neutral-200 tw-rounded-full tw-px-3 tw-py-1 tw-text-sm tw-font-semibold tw-text-neutral-700 tw-mr-2 mb-2"
+            >#fall</span
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -78,9 +45,13 @@ export default {
       type: String,
       required: true,
     },
-    background: {
+    buttonText: {
       type: String,
-      default: "#2B5CEA",
+      default: "View Project",
+    },
+    image: {
+      type: String,
+      required: true,
     },
     icon: {
       type: String,
@@ -94,6 +65,93 @@ export default {
 };
 </script>
 
-<style scoped>
-/* Add your custom styles here */
+<style lang="scss" scoped>
+.card-container {
+  // width: 300px;
+  // height: 300px;
+  position: relative;
+  border-radius: 10px;
+}
+
+.card-container::before {
+  content: "";
+  z-index: -1;
+  position: absolute;
+  inset: 0;
+  // background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100%);
+  transform: translate3d(0, 0, 0) scale(0.95);
+  filter: blur(20px);
+}
+
+.card {
+  width: 100%;
+  height: 100%;
+  border-radius: inherit;
+  overflow: hidden;
+}
+
+.card .img-content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100%);
+  // transition: scale 0.6s, rotate 0.6s, filter 1s;
+}
+
+.card .img-content img {
+  width: 100%;
+  height: 100%;
+  // fill: #e8e8e8;
+  filter: brightness(55%);
+  object-fit: cover;
+  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+.card .content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  // align-items: center;
+  justify-content: flex-end;
+  // text-align: center;
+  gap: 10px;
+  color: #e8e8e8;
+  padding: 20px;
+  line-height: 1.5;
+  border-radius: 5px;
+  opacity: 0;
+  // pointer-events: none;
+  // transform: translateY(50px);
+  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+
+  @apply hover:tw-animate-fade-up;
+}
+
+.card:hover .content {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.card:hover .img-content {
+  scale: 1.2;
+  filter: blur(1px);
+  @apply tw-animate-fade;
+}
+
+.title {
+  @apply tw-text-xl md:tw-text-2xl tw-font-bold tw-text-neutral-300 tw-mb-2 md:tw-mb-3;
+  // position: absolute;
+  // inset: auto auto 30px 30px;
+  text-transform: uppercase;
+}
+
+.description {
+  @apply tw-text-sm md:tw-text-base tw-text-neutral-300;
+}
 </style>
